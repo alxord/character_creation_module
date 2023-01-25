c75cf7d12fddefea1d7d4b1f5700507c85e60f6b
Александр Ноздрюхин <f1.alex2012@yandex.ru>
2023-01-26 00:39:08 +0300

    Add docstring PEP257

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+"""Документация модуля Описывает работу функций."""
+
+
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Атака(Имя_героя, Класс_героя)."""
     if char_class == 'warrior':
         return (
             f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}'
@@ -18,6 +23,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Защита(Имя_героя, Класс_героя)."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -28,6 +34,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Специальная атака(Имя_героя, Класс_героя)."""
     if char_class == 'warrior':
         return (f'{char_name}'
                 'применил специальное умение «Выносливость {80 + 25}»')
@@ -39,6 +46,12 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Тренировка.
+    Сначала идет проверка класса.
+    Используем команды:
+    attack(атака), defence(защита), special(спец_атака).
+    Для пропуска тренировки skip.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -63,6 +76,11 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбор класса.
+    Классы и их описания: warrior - воин,
+                          mage - маг,
+                          healer - лекарь.
+    """
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -86,7 +104,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> str:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
